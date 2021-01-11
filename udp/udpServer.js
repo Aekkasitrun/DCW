1a7088378956ea82de8f6c9d9ed5013c81be6a2b
Aekkasitrun <aekkasitrun@gmail.com>
2021-01-11 19:05:53 +0700

    0.33

--- udp/udpServer.js
+++ udp/udpServer.js
@@ -3,5 +3,5 @@ var server = dgram.createSocket("udp4");
 server.on("message", function (msg, rinfo) {
 console.log("server got: " + msg + " from " +
 rinfo.address + ":" + rinfo.port);
-});hgkghk
+});
 server.bind(41234); // server listening 0.0.0.0:41234
